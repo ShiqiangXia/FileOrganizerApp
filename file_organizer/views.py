c01abf51695ebcb1f7924f86245b237533e932d7
Shiqiang Xia <xiaxx268@umn.edu>
2021-07-02 12:52:53 -0500

    add move foler function

--- file_organizer/views.py
+++ file_organizer/views.py
@@ -9,8 +9,7 @@ from send2trash import send2trash
 import shutil
 import humanize
 
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QWidget, QFileDialog, QMessageBox, QInputDialog
 
 from .ui.window import Ui_Window
 
@@ -52,6 +51,8 @@ class Window(QWidget, Ui_Window):
         self.deleteBtn.clicked.connect(self.delete_file)
         self.undoBtn.clicked.connect(self.undo_file)
         self.doneBtn.clicked.connect(self.done_organize)
+        self.moveFolderList.itemDoubleClicked.connect(
+            self.move_to_selected_folder)
 
     def create_dir(self, current_dir, name):
         path = current_dir.absolute().as_posix() + '/' + name
@@ -70,7 +71,7 @@ class Window(QWidget, Ui_Window):
         file_size = humanize.naturalsize(entry.stat().st_size)
         self.infoText.setPlainText(
             f'Start organizing....\nFile {count}/{self._num_files},'
-            + f' Size {file_size}')
+            + f' Size: {file_size}')
         self.fileNameLabel.setText(file_name)
         # set file picture
         # TO DO
@@ -121,24 +122,27 @@ class Window(QWidget, Ui_Window):
         # set status to 'organizing'
 
     def skip_file(self):
-        entry = self._file_list[self._file_id]
-        self._file_id += 1
+        if self._file_id < self._num_files:
+            entry = self._file_list[self._file_id]
+            self._file_id += 1
 
-        file_path = entry.absolute().as_posix()
-        self._target_path.append(file_path)
-        self._action_list.append('skip')
+            file_path = entry.absolute().as_posix()
+            self._target_path.append(file_path)
+            self._action_list.append('skip')
 
-        self.print_file_info()
-
+            self.print_file_info()
+        
+    
     def delete_file(self):
-        entry = self._file_list[self._file_id]
-        self._file_id += 1
+        if self._file_id < self._num_files:
+            entry = self._file_list[self._file_id]
+            self._file_id += 1
 
-        file_path = entry.absolute().as_posix()
-        self.move_file(file_path, self._trash_path)
-        self._action_list.append('delete')
+            file_path = entry.absolute().as_posix()
+            self.move_file(file_path, self._trash_path)
+            self._action_list.append('delete')
 
-        self.print_file_info()
+            self.print_file_info()
   
     def undo_file(self):
         # check if undo is possible
@@ -147,21 +151,55 @@ class Window(QWidget, Ui_Window):
             action = self._action_list.pop()
             temp_path = self._target_path.pop()
             self._file_id -= 1
-
-            if action == 'delete' or 'move':
-                self.move_file(temp_path, self._folder_dir)
-                         
+            print('undo: ' + action)
+            print('temp_path: ' + temp_path)
+            if (action == 'delete') or (action == 'move'):
+                shutil.move(temp_path, self._folder_dir)
         else:
-            print('Undo is not possible!')
+            QMessageBox.warning(self, 'Warning', 'No more undo action!')
 
         self.print_file_info()
        
     def done_organize(self):
         # move trash_bin to real trash folder
         send2trash(self._trash_path)
-        trash_dir = Path(self._trash_path)
-        # for f in trash_dir.iterdir():
-        #     send2trash(f.absolute().as_posix())
-        trash_dir.rmdir()
+        # trash_dir = Path(self._trash_path)
+        # # for f in trash_dir.iterdir():
+        # #     send2trash(f.absolute().as_posix())
+        # trash_dir.rmdir()
 
         # set status to 'done'
+    
+    def move_to_selected_folder(self, item):
+        print(item.text())
+
+        if item.text() == '+ Add folder':
+            # add folder to move list
+            # get folder name
+            folder_name, ok  = QInputDialog.getText(
+                self, 'Input folder name', 'Folder name:')
+            if ok:
+                entry = self._file_list[self._file_id]
+                self._file_id += 1
+                file_path = entry.absolute().as_posix()
+                # create folder
+                folder_path = self.create_dir(self._folder_dir, folder_name)
+                # move file to folder
+                self.move_file(file_path, folder_path)
+                self._action_list.append('move')
+                # add folder to moveFolder List
+                self.moveFolderList.addItem(folder_name)
+        else:
+            # move file to selected folder
+            entry = self._file_list[self._file_id]
+            self._file_id += 1
+            file_path = entry.absolute().as_posix()
+            folder_name = item.text()
+            folder_path = self._folder_path + '/' + folder_name
+            self.move_file(file_path, folder_path)
+            self._action_list.append('move')
+        
+        self.print_file_info()
+        
+        
+        
