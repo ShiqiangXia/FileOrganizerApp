88706cb02f89d05de2db56bc7c696d69dc8156f9
Shiqiang Xia <xiaxx268@umn.edu>
2021-07-02 01:10:45 -0500

    start button implement

--- file_organizer/views.py
+++ file_organizer/views.py
@@ -12,8 +12,17 @@ from PyQt5.QtWidgets import QFileDialog
 
 from .ui.window import Ui_Window
 
-RECENT_DAYS  = 30
-most_recent = datetime.now() - timedelta(days = RECENT_DAYS)
+RECENT_DAYS = 30
+most_recent = datetime.now() - timedelta(days=RECENT_DAYS)
+
+def create_dir(current_dir, name):
+    path = current_dir.absolute().as_posix() + '/' + name
+    p = Path(path)
+    try:
+        p.mkdir()
+    except FileExistsError:
+        print('Folder %s already exists!'%name)
+    return(path)
 
 class Window(QWidget, Ui_Window):
     def __init__(self):
@@ -28,6 +37,11 @@ class Window(QWidget, Ui_Window):
         self._folder_size = 0
         self._num_recent_files = 0
 
+        self._move_dirs_path = []
+        self._move_dirs_name = []
+        self._action_list = []
+        self._target_path = []
+
         self._setupUI()
         self._connectSignalsSlots()
 
@@ -36,7 +50,8 @@ class Window(QWidget, Ui_Window):
 
     def _connectSignalsSlots(self):
         self.openBtn.clicked.connect(self.open_folder)
-    
+        self.startBtn.clicked.connect(self.start_organize_file)
+
     def open_folder(self):
         if self.dirEdit.text():
             initDir = self.dirEdit.text()
@@ -49,6 +64,7 @@ class Window(QWidget, Ui_Window):
         self.dirEdit.setText(self._folder_path)
         self._folder_dir = Path(self._folder_path)
 
+        # get file list
         for entry in self._folder_dir.iterdir():
             if entry.is_file():
                 self._num_files += 1
@@ -59,13 +75,30 @@ class Window(QWidget, Ui_Window):
             else:
                 self._num_folders += 1
 
+        # get folder size
         for f in self._folder_dir.glob('**/*'):
             self._folder_size += f.stat().st_size
         natural_size = humanize.naturalsize(self._folder_size)
-        
+
         self.infoText.setPlainText(
             f'{self._num_files} files, {self._num_folders} folders,\n'
             + f'size: {natural_size}')
+
+    
+    def start_organize_file(self):
+
+        # create trash folder
+        trash_path = create_dir(self._folder_dir, 'trash_bin')
+        count = 1
+        self.infoText.setPlainText(f'File {count}/{self._num_files}')
+
+        while self._file_list:
+            entry = self._file_list.pop()
+            file_name = entry.name
+            file_path = entry.absolute().as_posix()
+            self.fileNameLabel.setText(file_name)
+
+
 
         
        
